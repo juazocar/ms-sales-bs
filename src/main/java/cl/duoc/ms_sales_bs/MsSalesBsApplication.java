333aa2ca5d486053920c5f5a757a5c12d8eac1e8
juazocar <juazocar@profesor.duoc.cl>
2025-04-10 17:10:35 -0400

    first commit for bs

---
+++ src/main/java/cl/duoc/ms_sales_bs/MsSalesBsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_sales_bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSalesBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSalesBsApplication.class, args);
+	}
+
+}
